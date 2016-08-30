bf4857db7d4d70ae274863d889b5ebecafcf28d1
Cameron Dubas <camerondubas@gmail.com>
2016-08-30 14:25:26 -0700

    Deploy Fixes

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/public'));
+app.get('*', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
 app.listen(process.env.PORT || 3000);
